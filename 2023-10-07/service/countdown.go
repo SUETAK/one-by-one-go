85d43aea98e0d7af97a748842cd25ed7c3c489c2
suetak <akasirotonbo08451@gmail.com>
2023-10-08 01:02:52 +0900

    [2023-10-07] キー監視を追加

--- 2023-10-07/service/countdown.go
+++ 2023-10-07/service/countdown.go
@@ -1,6 +1,11 @@
 package service
 
-import "2023-10-07/domain"
+import (
+	"2023-10-07/domain"
+	"fmt"
+	"github.com/eiannone/keyboard"
+	"time"
+)
 
 type CountdownService interface {
 	Start()
@@ -16,4 +21,34 @@ type countdownService struct {
 	timer *domain.Timer
 }
 
-func (c *countdownService) Start() {}
+func (c *countdownService) Start() {
+	// キーボードの監視を開始
+	err := keyboard.Open()
+	if err != nil {
+		panic(err)
+	}
+	defer keyboard.Close()
+
+	fmt.Println("10秒間のカウントダウンを開始します。途中で任意のキーを押してください。")
+
+	// タイマーを設定 (例: 10秒)
+	timerDuration := 10 * time.Second
+	endTime := time.Now().Add(timerDuration)
+
+	for time.Now().Before(endTime) {
+		// キーイベントの監視 (非ブロッキング)
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			panic(err)
+		}
+
+		if char != 0 {
+			fmt.Printf("'%c' キーが押されました。\n", char)
+		}
+
+		// 一定間隔（例: 0.1秒）で確認
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	fmt.Println("カウントダウン終了!")
+}
