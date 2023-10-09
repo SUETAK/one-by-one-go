1fa2a2b67ff66fd05db37f44a4ede0e2bedbb582
suetak <akasirotonbo08451@gmail.com>
2023-10-09 19:26:33 +0900

    [2023-10-09] state からカウントダウンをするかを判断する

--- 2023-10-08-9/service/countdown.go
+++ 2023-10-08-9/service/countdown.go
@@ -29,7 +29,7 @@ func (c *countdownService) Start() {
 	}
 	defer keyboard.Close()
 
-	fmt.Printf("%d秒間のカウントダウンを設定しました。\n", c.timer.GetDuration())
+	fmt.Printf("%d秒間のカウントダウンを設定しました。\n カウントを開始するためには't'キーを押してください。 カウントを中止する時は's'、再開する時は'r'キーを押してください", c.timer.GetDuration())
 
 	keyPressCh := make(chan rune)
 	go func() {
@@ -42,25 +42,24 @@ func (c *countdownService) Start() {
 		}
 	}()
 
-	timerRunning := false
-
 	for c.timer.GetDuration() >= 0 {
 		select {
 		case char := <-keyPressCh:
 			switch char {
 			case 't':
-				timerRunning = true
+				c.timer.CountStart()
+				fmt.Println("カウントダウンを開始しました")
 			case 's':
-				timerRunning = false
+				c.timer.CountStop()
 				fmt.Println("カウントダウンを停止しました")
 			case 'r':
-				timerRunning = true
+				c.timer.CountStart()
 				fmt.Println("カウントダウンを再開しました")
 			default:
 				fmt.Printf("'%c' キーが押されました。\n", char)
 			}
 		default:
-			if timerRunning {
+			if c.timer.GetState() == "start" {
 				c.timer.Countdown()
 				fmt.Printf("%d秒\n", c.timer.GetDuration())
 				time.Sleep(1 * time.Second)
