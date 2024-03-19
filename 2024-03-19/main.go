16a4f464cfaeb7c9cc7d32ad9e35598c752536c2
suetak <akasirotonbo08451@gmail.com>
2024-03-20 00:17:33 +0900

    TopInterview150-2

--- 2024-03-19/main.go
+++ 2024-03-19/main.go
@@ -3,12 +3,13 @@ package main
 func main() {}
 
 func removeElement(nums []int, val int) int {
-	var removedNums []int
+	var answer int
 	for _, v := range nums {
 		if v != val {
-			removedNums = append(removedNums, v)
+			nums[answer] = v
+			answer++
 		}
 	}
-	nums = removedNums
-	return len(removedNums)
+
+	return answer
 }
