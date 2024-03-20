c12d70452b28204e4e866ebe2581a0b538664028
suetak <akasirotonbo08451@gmail.com>
2024-03-21 01:51:19 +0900

    TopInterview150-3 解答追加

--- 2024-03-20/main.go
+++ 2024-03-20/main.go
@@ -1,3 +1,16 @@
 package main
 
 func main() {}
+
+func removeDuplicates(nums []int) int {
+	k := 1
+	previousValue := nums[0]
+	for _, v := range nums {
+		if v != previousValue {
+			nums[k] = v
+			k++
+			previousValue = v
+		}
+	}
+	return k
+}
---
+++ 2024-03-20/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  int
+		want2 []int
+	}{
+		{
+			name:  "test1",
+			args:  args{nums: []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}},
+			want:  5,
+			want2: []int{0, 1, 2, 3, 4, 2, 2, 3, 3, 4},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			nums := tt.args.nums
+			if got := removeDuplicates(nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+			for i, v := range nums[:5] {
+				if v != tt.want2[i] {
+					t.Errorf("removeDuplicates() = %v, want %v", v, tt.want2[i])
+				}
+			}
+		})
+	}
+}
