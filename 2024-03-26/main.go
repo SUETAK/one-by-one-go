388a2dd66209be935c422614920eb157b6c3d3b7
suetak <akasirotonbo08451@gmail.com>
2024-03-27 01:37:56 +0900

    最速解答を追加

--- 2024-03-26/main.go
+++ 2024-03-26/main.go
@@ -3,7 +3,6 @@ package main
 func main() {}
 
 func rotate(nums []int, k int) {
-
 	separate := k % len(nums)
 	reverse(nums, 0, len(nums)-1)
 	reverse(nums, 0, separate-1)
@@ -17,4 +16,15 @@ func reverse(nums []int, start int, end int) {
 		start++
 		end--
 	}
+}
+
+func rotateBest(nums []int, k int) {
+	num := len(nums)
+	k = k % num
+	temp := make([]int, 0, num)
+	// num-k を分割する箇所として考えられればOKだった
+	left := append(temp, nums[num-k:]...)
+	temp = append(left, nums[:num-k]...)
+
+	copy(nums, temp)
 }
