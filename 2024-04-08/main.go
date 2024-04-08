b9578fffbcfada1dbc99be28cf7d1a100dbc6c84
suetak <akasirotonbo08451@gmail.com>
2024-04-08 23:13:00 +0900

    理解

--- 2024-04-08/main.go
+++ 2024-04-08/main.go
@@ -8,14 +8,19 @@ func productExceptSelf(nums []int) []int {
 	answer[len(nums)-1] = 1
 
 	// 各要素の一つ左の要素を掛け算する
+	// 一番左は左がないのでi=1 から始める
 	for i := 1; i < len(nums); i++ {
+		// answer のi-1(1つ左)は左側の累積になっている
 		answer[i] = answer[i-1] * nums[i-1]
 	}
 
 	rightProduct := 1
-	for i := len(nums) - 2; i >= 0; i-- {
-		rightProduct *= nums[i+1]
-		answer[i] *= rightProduct
+	// len(nums)-1 が配列の最後のindex
+	// 一番右より右はないので最後のindex から-1 する
+	lastIndex := len(nums) - 1
+	for i := lastIndex - 1; i >= 0; i-- {
+		rightProduct *= nums[i+1] // 累積
+		answer[i] *= rightProduct // 左側の累積*右側の累積
 	}
 	return answer
 }
