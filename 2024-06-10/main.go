fb7a2fd5e2fb033cd6e507ef4a727cac46eacb3a
suetak <akasirotonbo08451@gmail.com>
2024-06-11 00:00:41 +0900

    functional パターンを実装する

--- 2024-06-10/main.go
+++ 2024-06-10/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"log"
 	"one-by-one-go/2024-06-10/server"
+	"os"
+	"time"
 )
 
 func main() {
-	// 引数が可変じゃないので、複数の値を入れられない
-	svr := server.New("localhost", 8888)
+	l, err := os.Create("server.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer l.Close()
+	logger := log.New(l, "", log.LstdFlags)
+
+	// 可変長引数にserver から呼び出したlogger, timeout を作成する関数を呼び出す
+	// WithLogger, WithTimeout は引数をserver のフィールドに対して上書きする関数を返す。
+	svr := server.New("localhost", 8888, server.WithLogger(logger), server.WithTimeout(time.Minute))
 
 	if err := svr.Start(); err != nil {
 		log.Fatal(err)
--- 2024-06-10/server/server.go
+++ 2024-06-10/server/server.go
@@ -1,17 +1,47 @@
 package server
 
+import (
+	"log"
+	"time"
+)
+
 type Server struct {
-	host string
-	port int
+	host    string
+	port    int
+	timeout time.Duration
+	logger  *log.Logger
 }
 
-func New(host string, port int) *Server {
-	return &Server{
+func New(host string, port int, option ...Option) *Server {
+	svr := &Server{
 		host: host,
 		port: port,
 	}
+
+	// 可変長引数をfor文で回し、関数を実行。引数にsvr を渡すことでsvr を更新する
+	for _, opt := range option {
+		opt(svr)
+	}
+
+	return svr
 }
 
 func (s Server) Start() error {
 	return nil
+}
+
+// server を引数に取る関数に名前をつける
+type Option func(server *Server)
+
+// Option を返すようにして、引数のserver を更新する関数を返す
+func WithTimeout(timeout time.Duration) Option {
+	return func(s *Server) {
+		s.timeout = timeout
+	}
+}
+
+func WithLogger(logger *log.Logger) Option {
+	return func(s *Server) {
+		s.logger = logger
+	}
 }
